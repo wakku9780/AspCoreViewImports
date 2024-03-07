5bd7122871b130bbb461a33870c623ee500bd70f
waqar <wakku9780@gmail.com>
2024-03-07 23:37:45 +0530

    Add project files.

---
+++ AspCoreViewImports/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿
+using AspCoreViewImports.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspCoreViewImports.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{id=1,Name ="Waqar",Gender="Male"},
+                new Student{id=2,Name ="Abdul",Gender="Male"},
+                new Student{id=3,Name ="Tania",Gender="Female"},
+                new Student{id=4,Name ="Kaveri",Gender="Female" },
+                new Student{id=5,Name ="Hashim",Gender="Male" },
+            };
+            return View(students);
+        }
+
+        public IActionResult About()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{id=1,Name ="Waqar",Gender="Male"},
+                new Student{id=2,Name ="Abdul",Gender="Male"},
+                new Student{id=3,Name ="Tania",Gender="Female"},
+                new Student{id=4,Name ="Kaveri",Gender="Female" },
+                new Student{id=5,Name ="Hashim",Gender="Male" },
+            };
+            return View(students);
+        }
+
+        public IActionResult Contact()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{id=1,Name ="Waqar",Gender="Male"},
+                new Student{id=2,Name ="Abdul",Gender="Male"},
+                new Student{id=3,Name ="Tania",Gender="Female"},
+                new Student{id=4,Name ="Kaveri",Gender="Female" },
+                new Student{id=5,Name ="Hashim",Gender="Male" },
+            };
+            return View(students);
+        }
+    }
+}
---
+++ AspCoreViewImports/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspCoreViewImports.Models
+{
+    public class Student  
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+
+    }
+}
